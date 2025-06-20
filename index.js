cac86424d5495ed438ed3fb9ff9f1dc59febd9c2
moudud <sihamislam18@gmail.com>
2025-06-20 16:29:09 +0600

    response the server clinet project

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app=express();
 const cors = require('cors');
 require('dotenv').config()
+const { ObjectId } = require('mongodb');
 const port=process.env.PORT||5000;
 app.use(cors());
 app.use(express.json());
@@ -9,7 +10,7 @@ app.use(express.json());
 // DB_PASS=wyuJpyXhuph9XaaU
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.leope.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -50,6 +51,12 @@ app.post('/cart',async(req,res)=>{
     const cartItem=req.body;
     const result=await cartCollection.insertOne(cartItem);
     res.send(result)
+})
+app.delete('/cart/:id',async(req,res)=>{
+const id=req.params.id
+const query={_id:new ObjectId(id)}
+const result=await cartCollection.deleteOne(query)
+res.send(result)
 })
 // review
 app.get("/review",async(req,res)=>{
